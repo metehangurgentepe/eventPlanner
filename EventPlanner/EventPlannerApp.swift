7e1d3f75cf3ba846af78984f25eb3814f8bb9abb
metehangurgentepe <metehangurgentepe@gmail.com>
2023-07-03 16:55:33 +0300

    Initial Commit

---
+++ EventPlanner/EventPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventPlannerApp.swift
+//  EventPlanner
+//
+//  Created by Metehan Gürgentepe on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct EventPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
